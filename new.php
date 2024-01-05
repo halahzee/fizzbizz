88035a5a9124dd4ec22bac62f2075ddece4bd3e3
halaha <halahzee@gmail.com>
2024-01-04 18:24:40 -0600

    add BUZZ and FIZZBIZZ

--- new.php
+++ new.php
@@ -3,14 +3,19 @@ echo "<h1>PHP</h1>";
 
 for ($i = 1; $i <= 100; $i++)
 {
-  if ( $i% 15 == 0)
+  if ( $i% 15 == 0 )
    {
      echo $i . " BUZZZ". "<br>" ;
    }
-  else if ( $i% 4 == 0 ) 
+  if ( $i% 15 == 0)
+   {
+     echo $i . " FIZZBIZZ". "<br>" ;
+   }
+    else if ( $i% 4 == 0 ) 
    {
      echo $i. " Bizz"."<br>";
    }
+ 
      else if ( $i% 2 == 0 ) 
    {
      echo $i. " Fizz"."<br>";
